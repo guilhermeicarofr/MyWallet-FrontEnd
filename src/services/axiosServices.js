beaa4c8599d1f78ed87af0a0a356b82e68f617ae
guilhemeicarofr <guilhemeicarofr@gmail.com>
2022-09-14 05:14:01 -0300

    enviroment variable config

--- src/services/axiosServices.js
+++ src/services/axiosServices.js
@@ -1,37 +1,40 @@
 import axios from 'axios';
 
+const URL = process.env.BACKEND_URL;
+console.log(URL);
+
 function signIn(body) {
-    const promise = axios.post(`${process.env.BASE_URL}/signin`, body);
+    const promise = axios.post(`${URL}/signin`, body);
     return promise;
 }
 
 function signUp(body) {
-    const promise = axios.post(`${process.env.BASE_URL}/signup`, body);
+    const promise = axios.post(`${URL}/signup`, body);
     return promise;
 }
 
 function getUserName(token) {
-    const promise = axios.get(`${process.env.BASE_URL}/name`, { headers: {'Authorization':`Bearer ${token}`} });
+    const promise = axios.get(`${URL}/name`, { headers: {'Authorization':`Bearer ${token}`} });
     return promise;
 }
 
 function getUserBalance(token) {
-    const promise = axios.get(`${process.env.BASE_URL}/balance`, { headers: {'Authorization':`Bearer ${token}`} });
+    const promise = axios.get(`${URL}/balance`, { headers: {'Authorization':`Bearer ${token}`} });
     return promise;
 }
 
 function getTransactions(token) {
-    const promise = axios.get(`${process.env.BASE_URL}/transactions`, { headers: {'Authorization':`Bearer ${token}`} });
+    const promise = axios.get(`${URL}/transactions`, { headers: {'Authorization':`Bearer ${token}`} });
     return promise;
 }
 
 function postTransaction(token, body) {
-    const promise = axios.post(`${process.env.BASE_URL}/transactions`, body, { headers: {'Authorization':`Bearer ${token}`} });
+    const promise = axios.post(`${URL}/transactions`, body, { headers: {'Authorization':`Bearer ${token}`} });
     return promise;
 }
 
 function deleteTransaction(token, id) {
-    const promise = axios.delete(`${process.env.BASE_URL}/transactions/${id}`, { headers: {'Authorization':`Bearer ${token}`} });
+    const promise = axios.delete(`${URL}/transactions/${id}`, { headers: {'Authorization':`Bearer ${token}`} });
     return promise;
 }
 
