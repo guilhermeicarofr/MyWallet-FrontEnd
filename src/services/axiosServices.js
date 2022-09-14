c44b1b2b519c1d16f3314fc40d1e77f672a5b217
guilhemeicarofr <guilhemeicarofr@gmail.com>
2022-09-14 03:50:20 -0300

    process changes to heroku backend linked deploy with vercel

--- src/services/axiosServices.js
+++ src/services/axiosServices.js
@@ -1,39 +1,40 @@
 import axios from 'axios';
+import dotenv from 'dotenv';
 
-const BASE_URL = 'http://localhost:5000';
+dotenv.config();
 
 function signIn(body) {
-    const promise = axios.post(`${BASE_URL}/signin`, body);
+    const promise = axios.post(`${process.env.BASE_URL}/signin`, body);
     return promise;
 }
 
 function signUp(body) {
-    const promise = axios.post(`${BASE_URL}/signup`, body);
+    const promise = axios.post(`${process.env.BASE_URL}/signup`, body);
     return promise;
 }
 
 function getUserName(token) {
-    const promise = axios.get(`${BASE_URL}/name`, { headers: {'Authorization':`Bearer ${token}`} });
+    const promise = axios.get(`${process.env.BASE_URL}/name`, { headers: {'Authorization':`Bearer ${token}`} });
     return promise;
 }
 
 function getUserBalance(token) {
-    const promise = axios.get(`${BASE_URL}/balance`, { headers: {'Authorization':`Bearer ${token}`} });
+    const promise = axios.get(`${process.env.BASE_URL}/balance`, { headers: {'Authorization':`Bearer ${token}`} });
     return promise;
 }
 
 function getTransactions(token) {
-    const promise = axios.get(`${BASE_URL}/transactions`, { headers: {'Authorization':`Bearer ${token}`} });
+    const promise = axios.get(`${process.env.BASE_URL}/transactions`, { headers: {'Authorization':`Bearer ${token}`} });
     return promise;
 }
 
 function postTransaction(token, body) {
-    const promise = axios.post(`${BASE_URL}/transactions`, body, { headers: {'Authorization':`Bearer ${token}`} });
+    const promise = axios.post(`${process.env.BASE_URL}/transactions`, body, { headers: {'Authorization':`Bearer ${token}`} });
     return promise;
 }
 
 function deleteTransaction(token, id) {
-    const promise = axios.delete(`${BASE_URL}/transactions/${id}`, { headers: {'Authorization':`Bearer ${token}`} });
+    const promise = axios.delete(`${process.env.BASE_URL}/transactions/${id}`, { headers: {'Authorization':`Bearer ${token}`} });
     return promise;
 }
 
